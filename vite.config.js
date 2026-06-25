71470e6ba783150761ebfc234fa630846d929f09
Yegor <test@gmail.com>
2026-06-25 20:41:20 +0300

    Update Vite configuration to ensure output directory is set and emptied before build

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,12 @@ import { defineConfig } from "vite";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  base: '/ceramic-soul/', 
+  base: '/ceramic-soul/',
 
   build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
