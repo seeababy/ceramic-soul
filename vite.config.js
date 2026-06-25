e7c33039859bb78bd14255adcc7828439426a48f
Yegor <test@gmail.com>
2026-06-25 19:37:23 +0300

    Refactor vite.config.js to clean up commented code and adjust input configuration

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,15 @@
-// import { dirname, resolve } from "node:path";
-// import { fileURLToPath } from "node:url";
-// import { defineConfig } from "vite";
-
-// const __dirname = dirname(fileURLToPath(import.meta.url));
-
-// export default defineConfig({
-// 	build: {
-// 		rollupOptions: {
-// 			input: {
-// 				main: resolve(__dirname, "index.html"),
-// 				catalog: resolve(__dirname, "catalog.html"),
-// 			},
-// 		},
-// 	},
-// });
-
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { defineConfig } from 'vite';
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-	base: '/ceramic-soul/',
-
 	build: {
 		rollupOptions: {
 			input: {
-				index: resolve(__dirname, 'index.html'),
-				catalog: resolve(__dirname, 'catalog.html'),
-				about: resolve(__dirname, 'about.html'),
-				blog: resolve(__dirname, 'blog.html'),
+				main: resolve(__dirname, "index.html"),
+				catalog: resolve(__dirname, "catalog.html"),
 			},
 		},
 	},
