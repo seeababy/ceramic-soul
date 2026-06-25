2d3c3699cdf7c8ebd8421bb91e4cdb7d23438eba
Yegor <test@gmail.com>
2026-06-25 21:30:25 +0300

    Fix formatting in Vite configuration for consistency

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
@@ -5,12 +6,9 @@ import { defineConfig } from "vite";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  base: '/ceramic-soul/',
+  base: '/ceramic-soul/', 
 
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
