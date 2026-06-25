4831194055361fa7974eb2bfcdaf3666f7dfcc86
Yegor <test@gmail.com>
2026-06-25 20:12:15 +0300

    Add initial HTML files for blog, catalog, and index pages with structured layout and content

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,23 @@
+// import { dirname, resolve } from "node:path";
+// import { fileURLToPath } from "node:url";
+// import { defineConfig } from "vite";
+
+// const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// export default defineConfig({
+// 	build: {
+// 		rollupOptions: {
+// 			input: {
+// 				main: resolve(__dirname, "index.html"),
+// 				catalog: resolve(__dirname, "catalog.html"),
+// 				blog: resolve(__dirname, "blog.html"),
+// 				about: resolve(__dirname, "about.html"),
+// 			},
+// 		},
+// 	},
+// });
+
+
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
@@ -5,14 +25,16 @@ import { defineConfig } from "vite";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-	build: {
-		rollupOptions: {
-			input: {
-				main: resolve(__dirname, "index.html"),
-				catalog: resolve(__dirname, "catalog.html"),
-				blog: resolve(__dirname, "blog.html"),
-				about: resolve(__dirname, "about.html"),
-			},
-		},
-	},
+  base: "./", 
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        catalog: resolve(__dirname, "catalog.html"),
+        blog: resolve(__dirname, "blog.html"),
+        about: resolve(__dirname, "about.html"),
+      },
+    },
+  },
 });
